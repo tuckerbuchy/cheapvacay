3d0acb7ac7c3eb2b4c5f12ff9523f32f9c1a0b2f
Tucker Buchy <tucker.buchy@gmail.com>
2017-08-29 10:58:37 -0700

    added

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from flask import Flask, request, render_template
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import pymongo
 
@@ -73,4 +73,4 @@ def search():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=port)
