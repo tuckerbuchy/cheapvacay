e0c8259d1463b416b2cb7260d64193bfcf95ca8d
Tucker Buchy <tucker.buchy@gmail.com>
2017-08-29 10:55:02 -0700

    added port

--- app.py
+++ app.py
@@ -72,4 +72,5 @@ def search():
     return json.dumps(calculated_travel_costs)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
